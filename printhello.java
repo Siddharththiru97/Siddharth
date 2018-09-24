6383637a44366e0e266549c567a70efcfb922f4c
Siddharththiru97 <42763543+Siddharththiru97@users.noreply.github.com>
2018-09-24 14:04:30 +0530

    Create printhello.java

---
+++ printhello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args)
+   {
+     int n;
+     System.out.print("");
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     for (int i = 0; i < n; i++)
+      {
+			System.out.println
+      ("Hello");
+      }
+  }
+}
